e8e4b7ef1ad71f0b81d62c4e668c29599f7ae48f
inigompare <inigosanz96@gmail.com>
2025-11-20 20:37:54 +0100

    t

--- Unidad 3 Parte 2/Ejercicio 15.py
+++ Unidad 3 Parte 2/Ejercicio 15.py
@@ -5,10 +5,3 @@ for i in range(altura, 0, -1):
     espacios = " " * (altura - i)
     asteriscos = "*" * (2 * i - 1)
     print(espacios + asteriscos)
-# Introducir datos
-altura = int(input("Introduce la altura de la pirámide: "))
-# Construir pirámide
-for i in range(1, altura + 1):
-    espacios = " " * (altura - i)
-    asteriscos = "*" * (2 * i - 1)
-    print(espacios + asteriscos)
